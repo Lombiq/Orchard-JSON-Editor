f076bf6cdc596c24973af60dd34bff77727b789b
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2021-07-24 17:36:07 +0200

    Fixing build errors

--- ResourceManagementOptionsConfiguration.cs
+++ ResourceManagementOptionsConfiguration.cs
@@ -1,25 +1,28 @@
+using Microsoft.Extensions.Options;
 using OrchardCore.ResourceManagement;
 using static Lombiq.JsonEditor.Constants.ResourceNames;
 
 namespace Lombiq.JsonEditor
 {
-    public class ResourceManifest : IResourceManifestProvider
+    public class ResourceManagementOptionsConfiguration : IConfigureOptions<ResourceManagementOptions>
     {
         private const string Vendors = "~/Lombiq.JsonEditor/vendors/";
 
-        public void BuildManifests(IResourceManifestBuilder builder)
-        {
-            var manifest = builder.Add();
+        private static readonly ResourceManifest _manifest = new();
 
-            manifest
+        static ResourceManagementOptionsConfiguration()
+        {
+            _manifest
                 .DefineScript(Library)
                 .SetUrl(Vendors + "jsoneditor/jsoneditor.min.js", Vendors + "jsoneditor/jsoneditor.js")
                 .SetVersion("9.5.0");
 
-            manifest
+            _manifest
                 .DefineStyle(Library)
                 .SetUrl(Vendors + "jsoneditor/jsoneditor.min.css", Vendors + "jsoneditor/jsoneditor.css")
                 .SetVersion("9.5.0");
         }
+
+        public void Configure(ResourceManagementOptions options) => options.ResourceManifests.Add(_manifest);
     }
 }
