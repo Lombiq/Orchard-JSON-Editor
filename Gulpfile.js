261bfe2aaea62ea7730aed4d75dfa5811abc5583
Dávid El-Saig <david.el-saig@lombiq.com>
2021-08-23 21:01:26 +0200

    Fix .gitignore and Gulpfile.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,4 +5,4 @@ const copyAssets = require('./Gulp/tasks/copy-assets');
 
 gulp.task('build:styles', scssTargets.build(paths.styles.base, paths.dist.css));
 gulp.task('copy:vendor-assets', () => copyAssets(paths.vendorAssets, paths.dist.vendors));
-gulp.task('default', gulp.parallel('build', 'copy:vendor-assets'));
+gulp.task('default', gulp.parallel('build:styles', 'copy:vendor-assets'));
