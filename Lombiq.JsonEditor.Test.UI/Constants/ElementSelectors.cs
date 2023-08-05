d7cfa61df8438e1435406e28b51ccb21691bc77c
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-08-05 19:35:57 +0200

    Removing unused Const folder

--- Lombiq.JsonEditor.Test.UI/Constants/ElementSelectors.cs
+++
@@ -1,7 +0,0 @@
-namespace Lombiq.JsonEditor.Tests.UI.Constants;
-
-public static class ElementSelectors
-{
-    public const string PrivacyConsentAcceptButtonId = "privacy-consent-accept-button";
-    public const string PrivacyConsentCheckboxLabelCss = "div.form-check > label";
-}
