181f7b66e4af64883f122d481740320e60e2e171
Szabolcs Deme <szabolcs.deme@lombiq.com>
2023-08-06 00:49:44 +0200

    Also testing if input is saved

--- Lombiq.JsonEditor.Test.UI/Extensions/TestCaseUITestContextExtensions.cs
+++ Lombiq.JsonEditor.Test.UI/Extensions/TestCaseUITestContextExtensions.cs
@@ -1,7 +1,9 @@
 using Atata;
 using Lombiq.Tests.UI.Extensions;
 using Lombiq.Tests.UI.Services;
+using Newtonsoft.Json.Linq;
 using OpenQA.Selenium;
+using Shouldly;
 using System.Threading.Tasks;
 
 namespace Lombiq.JsonEditor.Tests.UI.Extensions;
@@ -11,11 +13,14 @@ public static class TestCaseUITestContextExtensions
     private const string SampleContentItemId = "4xapn6ykttkk6wbbwgg1aaxqda";
     private const string HelloValue = "hello";
     private const string WorldValue = "world";
+    private const string TestField = "testField";
+    private const string TestValue = "testValue";
 
     private static readonly By ObjectByXPath = By.XPath($"//div[@class='jsoneditor-readonly' and contains(text(),'object')]");
-    private static readonly By ObjectCountByXPath = By.XPath($"//div[@class='jsoneditor-value jsoneditor-object' and contains(text(),'{{1}}')]");
+    private static readonly By ObjectCountByXPath = By.XPath($"//div[@class='jsoneditor-value jsoneditor-object' and contains(text(),'{{2}}')]");
     private static readonly By ArrayByXPath = By.XPath($"//div[@class='jsoneditor-field' and contains(text(),'printThese')]");
     private static readonly By ArrayCountByXPath = By.XPath($"//div[@class='jsoneditor-value jsoneditor-array' and contains(text(),'[2]')]");
+    private static readonly By FieldByXPath = By.XPath($"//div[@class='jsoneditor-field' and contains(text(), '{TestField}')]");
 
     public static async Task TestJsonEditorSampleBehaviorAsync(this UITestContext context)
     {
@@ -33,19 +38,35 @@ public static class TestCaseUITestContextExtensions
         await context.SignInDirectlyAsync();
         await context.GoToContentItemEditorByIdAsync(SampleContentItemId);
 
-        // Checking if the sample item is displayed correctly in the editor in tree editor mode.
-        await context.TestTreeTypeModeAsync();
+        // Testing if input is saved.
+        context.GetAll(By.XPath($"//button[@class='jsoneditor-button jsoneditor-contextmenu-button']"))[1].Click();
+        await context.ClickReliablyOnAsync(By.XPath($"//div[contains(text(),'Append')]"));
 
-        // Checking if the sample item is displayed correctly in the editor in tree view mode.
-        await context.SwitchToModeAsync("Switch to tree view");
-        await context.TestTreeTypeModeAsync();
+        context.Get(By.XPath($"//div[@class='jsoneditor-field jsoneditor-empty']")).FillInWith(TestField);
+        context.Get(By.XPath($"//div[@class='jsoneditor-value jsoneditor-string jsoneditor-empty']")).FillInWith(TestValue);
+        await context.ClickPublishAsync();
 
-        // Checking if the sample item is displayed correctly in the editor in form editor mode.
-        await context.SwitchToModeAsync("Switch to form editor");
-        await context.TestTreeTypeModeAsync();
+        // Checking if the sample item is displayed correctly in all tree style mode.
+        await context.TestTreeStyleModeAsync();
+
+        await context.SwitchToModeAsync("View");
+        await context.TestTreeStyleModeAsync();
+
+        await context.SwitchToModeAsync("Form");
+        await context.TestTreeStyleModeAsync();
+
+        // Checking if the sample item is displayed correctly in all code style mode.
+        await context.SwitchToModeAsync("Code");
+        context.TestCodeStyleMode();
+
+        await context.SwitchToModeAsync("Text");
+        context.TestCodeStyleMode();
+
+        await context.SwitchToModeAsync("Preview");
+        context.TestCodeStyleMode();
     }
 
-    private static void CheckArrayValueInTreeMode(this UITestContext context, string arrayValue, bool missing = false)
+    private static void CheckValueInTreeMode(this UITestContext context, string arrayValue, bool missing = false)
     {
         var arrayValueByXPath =
             By.XPath($"//div[@class='jsoneditor-value jsoneditor-string' and contains(text(),'{arrayValue}')]");
@@ -66,13 +87,13 @@ public static class TestCaseUITestContextExtensions
     private static Task ClickOnCollapseAllAsync(this UITestContext context) =>
         context.ClickReliablyOnAsync(By.XPath($"//button[@class='jsoneditor-collapse-all']"));
 
-    private static async Task SwitchToModeAsync(this UITestContext context, string editorTitle)
+    private static async Task SwitchToModeAsync(this UITestContext context, string editorName)
     {
         await context.ClickReliablyOnAsync(By.XPath($"//button[@class='jsoneditor-modes jsoneditor-separator']"));
-        await context.ClickReliablyOnAsync(By.XPath($"//button[@class='jsoneditor-type-modes' and @title ='{editorTitle}']"));
+        await context.ClickReliablyOnAsync(By.XPath($"//div[@class='jsoneditor-text' and contains(text(),'{editorName}')]"));
     }
 
-    private static async Task TestTreeTypeModeAsync(this UITestContext context)
+    private static async Task TestTreeStyleModeAsync(this UITestContext context)
     {
         await context.ClickOnExpandAllAsync();
 
@@ -84,9 +105,12 @@ public static class TestCaseUITestContextExtensions
         context.Exists(ArrayByXPath);
         context.Exists(ArrayCountByXPath);
 
-        // Checking "hello" and "word".
-        context.CheckArrayValueInTreeMode(HelloValue);
-        context.CheckArrayValueInTreeMode(WorldValue);
+        context.Exists(FieldByXPath);
+
+        // Checking "hello" and "word" and "testValue".
+        context.CheckValueInTreeMode(HelloValue);
+        context.CheckValueInTreeMode(WorldValue);
+        context.CheckValueInTreeMode(TestValue);
 
         // Collapse button should hide things.
         await context.ClickOnCollapseAllAsync();
@@ -97,8 +121,21 @@ public static class TestCaseUITestContextExtensions
         context.Missing(ArrayByXPath);
         context.Missing(ArrayCountByXPath);
 
-        context.CheckArrayValueInTreeMode(HelloValue, missing: true);
-        context.CheckArrayValueInTreeMode(WorldValue, missing: true);
+        context.Missing(FieldByXPath);
+
+        context.CheckValueInTreeMode(HelloValue, missing: true);
+        context.CheckValueInTreeMode(WorldValue, missing: true);
+        context.CheckValueInTreeMode(TestValue, missing: true);
     }
 
+    private static void TestCodeStyleMode(this UITestContext context)
+    {
+        // This field is hidden but it's content reflects what's in the editor.
+        var editorContent = JObject.Parse(context.Get(By.XPath($"//input[@class='jsonEditor__input']").OfAnyVisibility())
+        .GetValue());
+
+        ((string)editorContent["printThese"][0]).ShouldBe(HelloValue);
+        ((string)editorContent["printThese"][1]).ShouldBe(WorldValue);
+        ((string)editorContent[TestField]).ShouldBe(TestValue);
+    }
 }
