2958e73e82de1e8bdeab35f2643e511590734bf9
Sára El-Saig <sara.el-saig@lombiq.com>
2023-11-27 01:42:50 +0100

    Add model, viewmodel and controller.

---
+++ Lombiq.JsonEditor/Controllers/ContentController.cs
@@ -0,0 +1,36 @@
+﻿using Lombiq.JsonEditor.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OrchardCore.Admin;
+using OrchardCore.ContentManagement;
+using OrchardCore.Contents;
+using System.Threading.Tasks;
+
+namespace Lombiq.JsonEditor.Controllers;
+
+public class ContentController : Controller
+{
+    private readonly IContentManager _contentManager;
+    private readonly IAuthorizationService _authorizationService;
+
+    public ContentController(IContentManager contentManager, IAuthorizationService authorizationService)
+    {
+        _contentManager = contentManager;
+        _authorizationService = authorizationService;
+    }
+
+    [Admin]
+    [Route("/Contents/ContentItems/{contentItemId}/Edit/Json")]
+    public async Task<IActionResult> Edit(string contentItemId)
+    {
+        if (string.IsNullOrWhiteSpace(contentItemId) ||
+            await _contentManager.GetAsync(contentItemId, VersionOptions.Latest) is not { } contentItem ||
+            !await _authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, contentItem))
+        {
+            return NotFound();
+        }
+
+        return View(new EditContentItemViewModel(contentItem, JsonConvert.SerializeObject(contentItem)));
+    }
+}
---
+++ Lombiq.JsonEditor/ViewModels/EditContentItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿using OrchardCore.ContentManagement;
+
+namespace Lombiq.JsonEditor.ViewModels;
+
+public record EditContentItemViewModel(
+    string ContentItemId,
+    string DisplayText,
+    string Json)
+{
+    public EditContentItemViewModel(ContentItem contentItem, string json)
+        : this(contentItem.ContentItemId, contentItem.DisplayText, json)
+    {
+    }
+}
