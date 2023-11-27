cb6f93afbc427c65bcf8f91743f0e2dd9a30f4c1
Sára El-Saig <sara.el-saig@lombiq.com>
2023-11-27 01:57:02 +0100

    Post action.

--- Lombiq.JsonEditor/Controllers/ContentController.cs
+++ Lombiq.JsonEditor/Controllers/ContentController.cs
@@ -6,18 +6,24 @@ using OrchardCore.Admin;
 using OrchardCore.ContentManagement;
 using OrchardCore.Contents;
 using System.Threading.Tasks;
+using YesSql;
 
 namespace Lombiq.JsonEditor.Controllers;
 
 public class ContentController : Controller
 {
-    private readonly IContentManager _contentManager;
     private readonly IAuthorizationService _authorizationService;
+    private readonly IContentManager _contentManager;
+    private readonly ISession _session;
 
-    public ContentController(IContentManager contentManager, IAuthorizationService authorizationService)
+    public ContentController(
+        IAuthorizationService authorizationService,
+        IContentManager contentManager,
+        ISession session)
     {
-        _contentManager = contentManager;
         _authorizationService = authorizationService;
+        _contentManager = contentManager;
+        _session = session;
     }
 
     [Admin]
@@ -26,11 +32,37 @@ public class ContentController : Controller
     {
         if (string.IsNullOrWhiteSpace(contentItemId) ||
             await _contentManager.GetAsync(contentItemId, VersionOptions.Latest) is not { } contentItem ||
-            !await _authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, contentItem))
+            !await CanEditAsync(contentItem))
         {
             return NotFound();
         }
 
         return View(new EditContentItemViewModel(contentItem, JsonConvert.SerializeObject(contentItem)));
     }
+
+    [Admin]
+    [ValidateAntiForgeryToken]
+    [HttpPost, ActionName(nameof(Edit))]
+    public async Task<IActionResult> EditPost(string contentItemId, string json)
+    {
+        if (string.IsNullOrWhiteSpace(contentItemId) ||
+            string.IsNullOrWhiteSpace(json) ||
+            JsonConvert.DeserializeObject<ContentItem>(json) is not { } contentItem)
+        {
+            return NotFound();
+        }
+
+        if (string.IsNullOrWhiteSpace(contentItem.ContentItemId)) contentItem.ContentItemId = contentItemId;
+        if (!await CanEditAsync(contentItem))
+        {
+            return NotFound();
+        }
+
+        await _contentManager.PublishAsync(contentItem);
+        _session.Save(contentItem);
+        return RedirectToAction(nameof(Edit));
+    }
+
+    private Task<bool> CanEditAsync(ContentItem contentItem) =>
+        _authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, contentItem);
 }
