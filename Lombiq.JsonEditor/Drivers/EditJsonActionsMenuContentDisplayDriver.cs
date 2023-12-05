3735468fa0b3a7ed0b9120076af408195211fe91
Sára El-Saig <sara.el-saig@lombiq.com>
2023-12-05 05:33:09 +0100

    Menu item only if authorized.

--- Lombiq.JsonEditor/Drivers/EditJsonActionsMenuContentDisplayDriver.cs
+++ Lombiq.JsonEditor/Drivers/EditJsonActionsMenuContentDisplayDriver.cs
@@ -1,14 +1,30 @@
-﻿using OrchardCore.ContentManagement;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using OrchardCore.ContentManagement;
 using OrchardCore.ContentManagement.Display.ContentDisplay;
 using OrchardCore.ContentManagement.Display.ViewModels;
-using OrchardCore.DisplayManagement.ModelBinding;
+using OrchardCore.Contents;
+using OrchardCore.DisplayManagement.Handlers;
 using OrchardCore.DisplayManagement.Views;
+using System.Threading.Tasks;
 
 namespace Lombiq.JsonEditor.Drivers;
 
 public class EditJsonActionsMenuContentDisplayDriver : ContentDisplayDriver
 {
-    public override IDisplayResult Display(ContentItem model, IUpdateModel updater) =>
-        Initialize<ContentItemViewModel>("Content_EditJsonActions", viewModel => viewModel.ContentItem = model.ContentItem)
-            .Location("ActionsMenu:after");
+    private readonly IAuthorizationService _authorizationService;
+    private readonly IHttpContextAccessor _hca;
+
+    public EditJsonActionsMenuContentDisplayDriver(IAuthorizationService authorizationService, IHttpContextAccessor hca)
+    {
+        _authorizationService = authorizationService;
+        _hca = hca;
+    }
+
+    public override async Task<IDisplayResult> DisplayAsync(ContentItem model, BuildDisplayContext context) =>
+        await _authorizationService.AuthorizeAsync(_hca.HttpContext?.User, CommonPermissions.EditContent, model)
+            ? Initialize<ContentItemViewModel>("Content_EditJsonActions", viewModel =>
+                    viewModel.ContentItem = model.ContentItem)
+                .Location("ActionsMenu:after")
+            : null;
 }
