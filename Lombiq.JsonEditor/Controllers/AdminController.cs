979afa5f1d4ea9cd5886930a38853ffbaa9bfdf8
Sára El-Saig <sara.el-saig@lombiq.com>
2023-12-08 14:55:52 +0100

    Use ApiController.Post to update the content item.

--- Lombiq.JsonEditor/Controllers/AdminController.cs
+++ Lombiq.JsonEditor/Controllers/AdminController.cs
@@ -3,46 +3,55 @@ using Lombiq.HelpfulLibraries.OrchardCore.DependencyInjection;
 using Lombiq.JsonEditor.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.Localization;
 using Newtonsoft.Json;
 using OrchardCore.ContentManagement;
+using OrchardCore.ContentManagement.Metadata;
 using OrchardCore.Contents;
+using OrchardCore.Contents.Controllers;
 using OrchardCore.DisplayManagement;
 using OrchardCore.DisplayManagement.Layout;
+using OrchardCore.DisplayManagement.Notify;
 using OrchardCore.DisplayManagement.Title;
 using OrchardCore.Title.ViewModels;
 using System.Threading.Tasks;
-using YesSql;
 
 namespace Lombiq.JsonEditor.Controllers;
 
 public class AdminController : Controller
 {
     private readonly IAuthorizationService _authorizationService;
-    private readonly IContentItemIdGenerator _contentItemIdGenerator;
     private readonly IContentManager _contentManager;
+    private readonly IContentDefinitionManager _contentDefinitionManager;
     private readonly ILayoutAccessor _layoutAccessor;
+    private readonly INotifier _notifier;
     private readonly IPageTitleBuilder _pageTitleBuilder;
-    private readonly ISession _session;
     private readonly IShapeFactory _shapeFactory;
+    private readonly IStringLocalizer<ApiController> _apiStringLocalizer;
     private readonly IStringLocalizer<AdminController> T;
+    private readonly IHtmlLocalizer<AdminController> H;
 
     public AdminController(
-        IContentItemIdGenerator contentItemIdGenerator,
+        IContentDefinitionManager contentDefinitionManager,
         ILayoutAccessor layoutAccessor,
+        INotifier notifier,
         IPageTitleBuilder pageTitleBuilder,
         IShapeFactory shapeFactory,
-        IOrchardServices<AdminController> services)
+        IOrchardServices<AdminController> services,
+        IStringLocalizer<ApiController> apiStringLocalizer)
     {
         _authorizationService = services.AuthorizationService.Value;
-        _contentItemIdGenerator = contentItemIdGenerator;
         _contentManager = services.ContentManager.Value;
+        _contentDefinitionManager = contentDefinitionManager;
         _layoutAccessor = layoutAccessor;
+        _notifier = notifier;
         _pageTitleBuilder = pageTitleBuilder;
-        _session = services.Session.Value;
         _shapeFactory = shapeFactory;
+        _apiStringLocalizer = apiStringLocalizer;
         T = services.StringLocalizer.Value;
+        H = services.HtmlLocalizer.Value;
     }
 
     public async Task<IActionResult> Edit(string contentItemId)
@@ -54,10 +63,7 @@ public class AdminController : Controller
             return NotFound();
         }
 
-        var name = string.IsNullOrWhiteSpace(contentItem.DisplayText)
-            ? contentItem.ContentType
-            : $"\"{contentItem.DisplayText}\"";
-        var title = T["Edit {0} as JSON", name].Value;
+        var title = T["Edit {0} as JSON", GetName(contentItem)].Value;
         _pageTitleBuilder.AddSegment(new StringHtmlContent(title));
         var titleShape = await _shapeFactory.CreateAsync<TitlePartViewModel>("TitlePart", model =>
         {
@@ -92,15 +98,29 @@ public class AdminController : Controller
             return NotFound();
         }
 
-        if (await _contentManager.GetAsync(contentItem.ContentItemId, VersionOptions.Latest) is { } existing)
+        using var contentApiController = new ApiController(
+            _contentManager,
+            _contentDefinitionManager,
+            _authorizationService,
+            _apiStringLocalizer)
         {
-            existing.Latest = false;
-            _session.Save(existing);
-            contentItem.ContentItemVersionId = _contentItemIdGenerator.GenerateUniqueId(existing);
-        }
+            ControllerContext = { HttpContext = HttpContext },
+        };
 
-        contentItem.Published = false;
-        await _contentManager.PublishAsync(contentItem);
+        var result = await contentApiController.Post(contentItem);
+        switch (result)
+        {
+            case BadRequestObjectResult { Value: ValidationProblemDetails details }
+                when !string.IsNullOrWhiteSpace(details.Detail):
+                await _notifier.ErrorAsync(new LocalizedHtmlString(details.Detail, details.Detail));
+                return await Edit(contentItem.ContentItemId);
+            case OkObjectResult:
+                await _notifier.SuccessAsync(H["Content item {0} has been successfully saved.", GetName(contentItem)]);
+                break;
+            default:
+                await _notifier.ErrorAsync(H["The submission has failed, please try again."]);
+                return await Edit(contentItem.ContentItemId);
+        }
 
         if (!string.IsNullOrEmpty(returnUrl) &&
             submitPublish != "submit.PublishAndContinue" &&
@@ -114,4 +134,9 @@ public class AdminController : Controller
 
     private Task<bool> CanEditAsync(ContentItem contentItem) =>
         _authorizationService.AuthorizeAsync(User, CommonPermissions.EditContent, contentItem);
+
+    private static string GetName(ContentItem contentItem) =>
+        string.IsNullOrWhiteSpace(contentItem.DisplayText)
+            ? contentItem.ContentType
+            : $"\"{contentItem.DisplayText}\"";
 }
