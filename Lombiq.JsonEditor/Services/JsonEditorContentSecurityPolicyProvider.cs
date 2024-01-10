a121da1d8287cda3e12fa071c2a044da844e588b
Sára El-Saig <sara.el-saig@lombiq.com>
2024-01-10 23:54:45 +0100

    Also allow data:.

--- Lombiq.JsonEditor/Services/JsonEditorContentSecurityPolicyProvider.cs
+++ Lombiq.JsonEditor/Services/JsonEditorContentSecurityPolicyProvider.cs
@@ -1,18 +1,19 @@
 ﻿using Lombiq.JsonEditor.Constants;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
 using static Lombiq.HelpfulLibraries.AspNetCore.Security.ContentSecurityPolicyDirectives;
 using static Lombiq.HelpfulLibraries.AspNetCore.Security.ContentSecurityPolicyDirectives.CommonValues;
 
 namespace Lombiq.JsonEditor.Services;
 
 /// <summary>
-/// Permits <c>blob:</c> access to the <see cref="WorkerSrc"/> on pages that include the <see
+/// Permits <c>blob: data:</c> access to the <see cref="WorkerSrc"/> on pages that include the <see
 /// cref="ResourceNames.Library"/> script.
 /// </summary>
 public class JsonEditorContentSecurityPolicyProvider : ResourceManagerContentSecurityPolicyProvider
 {
     protected override string ResourceType => "script";
     protected override string ResourceName => ResourceNames.Library;
-    protected override string[] DirectiveNameChain { get; } = { WorkerSrc, ScriptSrc };
-    protected override string DirectiveValue => Blob;
+    protected override IReadOnlyCollection<string> DirectiveNameChain { get; } = new[] { WorkerSrc, ScriptSrc };
+    protected override string DirectiveValue => $"{Blob} {Data}";
 }
